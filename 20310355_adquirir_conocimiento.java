b79535db7ca24f9be04f6b79d2b2e0f279a2e0f1
Juan Cuevas <78194459+jcuevas340@users.noreply.github.com>
2023-09-12 19:27:15 -0600

    Update 20310355_adquirir_conocimiento.java

--- 20310355_adquirir_conocimiento.java
+++ 20310355_adquirir_conocimiento.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
 import java.util.Scanner;
+import java.io.*;
+import java.util.Map;
 
 public class ChatBot {
 
@@ -7,10 +9,7 @@ public class ChatBot {
 
     public ChatBot() {
         baseDeDatos = new HashMap<>();
-        baseDeDatos.put("Hola", "Hola, como estas?");
-        baseDeDatos.put("Como estas?", "Estoy bien, como te puedo ayudar?");
-        baseDeDatos.put("De que te gustaria hablar?",
-                "Podemos hablar sobre cualquier tema. Tienes alguna pregunta en mente");
+        cargarDatos(baseDeDatos);
     }
 
     public void iniciarChat() {
@@ -19,34 +18,48 @@ public class ChatBot {
         System.out.println(
                 "Hola, soy un Chatbot. Puedes preguntarme cualquier cosa. Escribe 'Salir' para terminar el chat.");
 
+        String preguntaNueva = "No sé la respuesta. ¿Qué te gustaría enseñarme?";
+
         while (true) {
             System.out.println("Tu: ");
             String pregunta = scanner.nextLine();
 
-            if (pregunta.equalsIgnoreCase("Salir")) {
-                System.out.println("Chatbot: Hasta luego!");
-                break;
-            }
-
-            String respuesta = obtenerRespuesta(pregunta);
-            if (respuesta != null) {
-                System.out.println("Chatbot: " + respuesta);
+            if (baseDeDatos.containsKey(pregunta)) {
+                String respuesta = baseDeDatos.get(pregunta);
             } else {
                 System.out.println("Chatbot: Lo siento, no se la respuesta. Cual es la respuesta adecuada?");
                 System.out.println("Tu (para ensenarme): ");
                 String respuestaNueva = scanner.nextLine();
-                agregarConocimiento(pregunta, respuestaNueva);
+                baseDeDatos.put(pregunta, respuestaNueva);
+                guardarDatos(baseDeDatos);
                 System.out.println("Chatbot: Gracias por ensenarme.");
             }
         }
     }
 
-    private String obtenerRespuesta(String pregunta) {
-        return baseDeDatos.get(pregunta);
+    private static void guardarDatos(Map<String, String> baseDeDatos) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("base_datos.txt"))) {
+            for (Map.Entry<String, String> entry : baseDeDatos.entrySet()) {
+                writer.write(entry.getKey() + " - " + entry.getValue());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void agregarConocimiento(String pregunta, String respuesta) {
-        baseDeDatos.put(pregunta, respuesta);
+    private static void cargarDatos(Map<String, String> baseDeDatos) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("base_datos.txt"))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                String[] partes = linea.split(" - ");
+                if (partes.length == 2) {
+                    baseDeDatos.put(partes[0], partes[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
